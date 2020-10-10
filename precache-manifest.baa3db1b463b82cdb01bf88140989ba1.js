522913800513f9ef7c585b9a6b633453f598aeab
Jacky Liang <jacky@stars-gaming.com>
2020-10-10 14:18:34 -0400

    Updates

---
+++ precache-manifest.baa3db1b463b82cdb01bf88140989ba1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e24ac73e47f0d307f9af1caca6c20c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e76850c25438c6fe7663",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "38d7cd2cf5a5bcbf5f2d",
+    "url": "/static/css/main.4a38a5ed.chunk.css"
+  },
+  {
+    "revision": "e76850c25438c6fe7663",
+    "url": "/static/js/2.29aa9831.chunk.js"
+  },
+  {
+    "revision": "b2af03aa0b5296f86a70c700e76455db",
+    "url": "/static/js/2.29aa9831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d7cd2cf5a5bcbf5f2d",
+    "url": "/static/js/main.b466c12d.chunk.js"
+  },
+  {
+    "revision": "3ae213438327ee80724f",
+    "url": "/static/js/runtime-main.155efc5d.js"
+  },
+  {
+    "revision": "800e172f32c3b64ae4d235b417ff6382",
+    "url": "/static/media/follow-bg.800e172f.jpg"
+  },
+  {
+    "revision": "7be79095fee17b49c06e11500ab172fa",
+    "url": "/static/media/footer-bg.7be79095.jpg"
+  },
+  {
+    "revision": "66bad97427fcc8e4daefd527bbc98726",
+    "url": "/static/media/intro-bg.66bad974.jpg"
+  },
+  {
+    "revision": "634fb019b4ec29646dbf3df2306cda00",
+    "url": "/static/media/logo.634fb019.png"
+  },
+  {
+    "revision": "3c96decde5c5eb53520cc7811ca42364",
+    "url": "/static/media/pysqd.3c96decd.jpg"
+  },
+  {
+    "revision": "e32392ea6ac324f8b0fa57f8970254f8",
+    "url": "/static/media/wechat-qr.e32392ea.jpg"
+  }
+]);
